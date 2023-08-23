36e81fd761d38e9ede3ce2259c7baf2b36e442bb
Sonja Janssen-Sahebzad <123073089+SonjaJanssen@users.noreply.github.com>
2023-08-23 23:12:50 +0200

    Add files via upload

---
+++ Codes Sentiment analysis of a poem Roses are sweet by Sonja Janssen-Sahebzad.R
@@ -0,0 +1,53 @@
+## Codes: by Sonja Janssen-Sahebzad
+## Codes of sentiment analysis of a poem called:
+## Poem: Roses are red. Violets are blue. Sugar is sweet, I love coding. And so do you
+## codes created in R.4.2.2 Rstudio
+## Date: 03-07-2023
+
+install.packages("syuzhet")
+install.packages("devtools")
+devtools::install_github("mjockers")
+install.packages("ggplot2")
+
+install.packages("geomtextpath")
+install.packages("Matrix")
+
+
+installed.packages()
+# Import libraries
+library(Matrix)
+library(geomtextpath)
+library(POET)
+library(sentiment.ai)
+library(tidyverse)
+
+library(syuzhet)
+library(devtools)
+library(mjockers)
+library(ggplot2)
+
+# Define the poem as a character vector
+poem <- c("Roses are red",
+          "Violets are blue",
+          "Sugar is sweet",
+          "I love coding and programming",
+          "And so do you")
+## code ok
+
+# Calculate the sentiment score for each line using syuzhet package
+sentiment <- get_sentiment(poem)
+## code ok
+
+# Create a data frame with the poem and the sentiment score
+data <- data.frame(poem, sentiment)
+## Code Ok
+
+# Plot the sentiment score using ggplot2 package
+ggplot(data, aes(x = 1:5, y = sentiment)) +
+  geom_col(fill = "green") +
+  geom_text(aes(label = poem), hjust = -0.1, size = 3) +
+  scale_x_continuous(breaks = 1:5) +
+  labs(x = "Line number", y = "Sentiment score", title = "Sentiment analysis of a poem by sonja Janssen-Sahebzad")
+
+## Zie Rplot 09 Poem Roses are red by Sonja Janssen-Sahebzad
+## codes ok
